c59fe9e93e10f8717d63881fdbfc7884974e9bfb
alex-115 <109692636+alex-115@users.noreply.github.com>
2025-02-11 20:56:00 -0800

    Update p1.c
    I added comments to the code

--- p1.c
+++ p1.c
@@ -19,15 +19,16 @@
 int lookup_and_connect(const char *host, const char *service);
 
 int main(int argc, char *argv[]) {
-    char *host = "www.ecst.csuchico.edu";
+    // initialization of variables used in the program
+    char *host = "www.ecst.csuchico.edu"; // assign the server host to a host variable
     int chunk_size;
-    int s;
-    char buf[MAX_CHUNK_SIZE];
+    int s; // socked descriptor
+    char buf[MAX_CHUNK_SIZE]; // buffer to store received data
     int bytes_received;
-    int total_bytes = 0;
-    int h1_count = 0;
-    const char *h1_tag = "<h1>";
-    const char *request = "GET /~kkredo/file.html HTTP/1.0\r\n\r\n";
+    int total_bytes = 0; // total bytes received
+    int h1_count = 0; // number of h1 tags
+    const char *h1_tag = "<h1>"; // h1 tag string to search
+    const char *request = "GET /~kkredo/file.html HTTP/1.0\r\n\r\n"; // http get request
 
     // Check command-line arguments
     if (argc != 2) {
@@ -40,7 +41,7 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
 
-    // Lookup IP and connect to server
+    // Lookup IP and connect to the server
     if ((s = lookup_and_connect(host, SERVER_PORT)) < 0) {
         exit(1);
     }
@@ -60,7 +61,7 @@ int main(int argc, char *argv[]) {
         char *ptr = buf;
         while ((ptr = strstr(ptr, h1_tag)) != NULL) {
             h1_count++;
-            ptr += strlen(h1_tag);
+            ptr += strlen(h1_tag); // move the pointer forward to avoid counting the same tag again
         }
     }
 
@@ -75,7 +76,7 @@ int main(int argc, char *argv[]) {
     printf("Number of <h1> tags: %d\n", h1_count);
     printf("Number of bytes: %d\n", total_bytes);
 
-    close(s);
+    close(s); // close the socket
     return 0;
 }
 
@@ -86,11 +87,11 @@ int lookup_and_connect(const char *host, const char *service) {
 
     // Translate host name into peer's IP address
     memset(&hints, 0, sizeof(hints));
-    hints.ai_family = AF_UNSPEC; // Use AF_UNSPEC instead of AF_INET
-    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_family = AF_UNSPEC; // Use AF_UNSPEC instead of AF_INET to allow ipv4 or ipv6
+    hints.ai_socktype = SOCK_STREAM; // use TCP
     hints.ai_flags = 0;
     hints.ai_protocol = 0;
-
+    // get address information
     if ((s = getaddrinfo(host, service, &hints, &result)) {
         fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
         return -1;
@@ -99,22 +100,22 @@ int lookup_and_connect(const char *host, const char *service) {
     // Iterate through the address list and try to connect
     for (rp = result; rp != NULL; rp = rp->ai_next) {
         if ((s = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol)) == -1) {
-            continue;
+            continue; // find the next address if socket creation fails
         }
 
         if (connect(s, rp->ai_addr, rp->ai_addrlen) != -1) {
-            break;
+            break; // successful connection
         }
 
-        close(s);
+        close(s); // close socket if connection fails
     }
 
-    if (rp == NULL) {
+    if (rp == NULL) { // no address 
         perror("connect");
-        freeaddrinfo(result);
+        freeaddrinfo(result); // free addrinfo
         return -1;
     }
 
-    freeaddrinfo(result);
-    return s;
+    freeaddrinfo(result); // free addrinfo
+    return s; // return the connected socket discriptor
 }
