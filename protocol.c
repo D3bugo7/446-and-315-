c13cdf3528f2f56400cfcb65e90611db6b433554
D3bugo7 <149905101+D3bugo7@users.noreply.github.com>
2025-03-03 21:45:28 -0800

    Update protocol.c

--- protocol.c
+++ protocol.c
@@ -5,14 +5,24 @@
 #include <arpa/inet.h>
 #include "protocol.h"
 
-// Example: Create a JOIN request
 void create_join_request(uint8_t *buffer, uint32_t peer_id) {
-    buffer[0] = JOIN_ACTION; // JOIN_ACTION defined as 0 in protocol.h
+    buffer[0] = JOIN_ACTION;
     uint32_t network_peer_id = htonl(peer_id);
     memcpy(buffer + 1, &network_peer_id, sizeof(network_peer_id));
 }
 
-// Example: Parse a SEARCH response
+void create_publish_request(uint8_t *buffer, int file_count, const char *filenames) {
+    buffer[0] = PUBLISH_ACTION;
+    uint32_t network_count = htonl(file_count);
+    memcpy(buffer + 1, &network_count, sizeof(network_count));
+    strcpy((char *)buffer + 5, filenames);
+}
+
+void create_search_request(uint8_t *buffer, const char *filename) {
+    buffer[0] = SEARCH_ACTION;
+    strcpy((char *)buffer + 1, filename);
+}
+
 int parse_search_response(const uint8_t *buffer, uint32_t *peer_id, char *peer_ip, uint16_t *peer_port) {
     *peer_id = ntohl(*(uint32_t *)buffer);
     struct in_addr addr;
@@ -21,5 +31,3 @@ int parse_search_response(const uint8_t *buffer, uint32_t *peer_id, char *peer_i
     *peer_port = ntohs(*(uint16_t *)(buffer + 8));
     return 0;
 }
-
-// more protocol functions will be added here.
