4c071c0c4b0850b1e4ecae04856acc5c6b02d69f
D3bugo7 <149905101+D3bugo7@users.noreply.github.com>
2025-03-03 21:47:02 -0800

    Update protocol.h

--- protocol.h
+++ protocol.h
@@ -1,4 +1,4 @@
-//protocol.h
+// protocol.h
 #ifndef PROTOCOL_H
 #define PROTOCOL_H
 #include <stdint.h>
@@ -9,6 +9,8 @@
 #define SEARCH_ACTION 2
 
 void create_join_request(uint8_t *buffer, uint32_t peer_id);
+void create_publish_request(uint8_t *buffer, int file_count, const char *filenames);
+void create_search_request(uint8_t *buffer, const char *filename);
 int parse_search_response(const uint8_t *buffer, uint32_t *peer_id, char *peer_ip, uint16_t *peer_port);
 
 #endif
